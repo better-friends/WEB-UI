be631d4d8ae6a9cfdaba445787e52d7c18c39660
Trevor-Lewis <tenzingtitan@gmail.com>
2019-05-01 16:41:23 -0700

    Animations with Javascript complete.

---
+++ customers.js
@@ -0,0 +1,5 @@
+// GSAP
+
+TweenLite.from('.full-card', 1, {opacity: 0, y: -1000})
+TweenLite.from('.font-card', 1, {opacity: 0, delay: 0.5 })
+TweenLite.from('.font-description', 1, {opacity: 0, x: 400, delay: 1})
